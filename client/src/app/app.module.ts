f0d5d631d4e99782c5d01e691295cbb45f304ec7
bpearce@angelo.edu <bpearce@angelo.edu>
2019-07-13 14:43:42 -0500

    more setup, created mock response

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 // Component Modules
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   imports: [ // modules
     BrowserModule,
+    HttpClientModule,
     MenuModule,
     CoreModule
   ],
--- server/express.js
+++ server/express.js
@@ -6,12 +6,23 @@ const port = process.env.PORT || 3000;
 
 const app = express()
 
+// temp
+const mockData = require('./mock/menuItem.json');
+
 // const middleware = require('./middleware')
 // const router = require('./router')
 
 // app.use(router.middleware())
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/:restaurant/menu', function (req, res) {
+    return res.status(200).json({
+        mockData
+    });
+});
+
+// start and listen to port
 app.listen(port, () => {
     console.log(`Service is up on port ${port}!`);
 });
