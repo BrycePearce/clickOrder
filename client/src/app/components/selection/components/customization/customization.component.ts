5b34bdf9db4ecc7afac694b7ca0c3ea54eb3195c
bpearce@angelo.edu <bpearce@angelo.edu>
2019-07-29 19:27:12 -0500

    minor style change

--- client/src/app/components/selection/components/customization/customization.component.ts
+++ client/src/app/components/selection/components/customization/customization.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customization.component.scss']
 })
 export class CustomizationComponent implements OnInit {
-
+  public customizationOptions = [1];
   constructor() { }
 
   ngOnInit() {
